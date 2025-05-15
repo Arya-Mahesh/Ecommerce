fa52f07d31e259fd65f118f31ebe60bee3bf61dd
Arya P K <aryamahesh769@gmail.com>
2025-05-15 16:12:37 +0530

    ProductCard - Created prodcut card

---
+++ plp/src/app/plp/catalog/[slug]/page.tsx
@@ -0,0 +1,9 @@
+import ProductCard from "@/components/productCard/productCard";
+
+export default function ProductPage( ) {
+  return (
+    <>
+      <ProductCard />
+    </>
+  );
+}
---
+++ plp/src/components/productCard/productCard.tsx
@@ -0,0 +1,5 @@
+
+export default function ProductCard() {
+  return (
+    <h1>This is Arya</h1>
+  )}
